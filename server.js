98a438aa776f48a8d9552c7d7df2937d3dba5903
RYAN <RTIPPER44@GMAIL.COM>
2021-01-16 16:27:16 -0800

    finished card 2

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 // ROUTES
 app.get('/', homeHandler);
 app.post('/contact', contactHandler);
+app.post('/searches', createSearch);
+
+// CATCH ALL
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 // HANDLERS FUNCTIONS
 function homeHandler(request, response) {
@@ -25,6 +29,20 @@ function contactHandler(request, response) {
   response.status(200).json('Thank You!');
 }
 
+function createSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  console.log(request.body);
+  console.log(request.body.search);
+
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+  superagent.get(url)
+  .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+  .then(results => response.render('pages/show', { searchResults: results }));
+
+}
+
 // PORT LISTEN
 app.listen(PORT, () => console.log(`now listening on port ${PORT}`));
-
